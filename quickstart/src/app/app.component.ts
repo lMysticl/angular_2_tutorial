8b2549bff5f80bc8807656323493a75c20aea5c8
Mystic <ufoman@bigmir.net>
2017-03-06 17:56:02 +0200

    first commit

--- quickstart/src/app/app.component.ts
+++ quickstart/src/app/app.component.ts
@@ -6,17 +6,19 @@ import { HeroService } from './hero.service';
 @Component({
   selector: 'my-app',
   template: `
+    <!--<h1>{{title}}</h1>-->
+    <!--<h2>My Heroes</h2>-->
+    <!--<ul class="heroes">-->
+      <!--<li *ngFor="let hero of heroes"-->
+        <!--[class.selected]="hero === selectedHero"-->
+        <!--(click)="onSelect(hero)">-->
+        <!--<span class="badge">{{hero.id}}</span> {{hero.name}}-->
+      <!--</li>-->
+    <!--</ul>-->
+    <!--<my-hero-detail [hero]="selectedHero"></my-hero-detail>-->
     <h1>{{title}}</h1>
-    <h2>My Heroes</h2>
-    <ul class="heroes">
-      <li *ngFor="let hero of heroes"
-        [class.selected]="hero === selectedHero"
-        (click)="onSelect(hero)">
-        <span class="badge">{{hero.id}}</span> {{hero.name}}
-      </li>
-    </ul>
-    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
-    
+    <a routerlink="/heroes">Heroes</a>
+    <router-outlet></router-outlet>
   `,
   providers: [HeroService],
   styles: [`
