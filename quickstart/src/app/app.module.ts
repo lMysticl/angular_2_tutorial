b8342c0ff8be1496279d7da0cd8bdd3f8b99236b
Mystic <ufoman@bigmir.net>
2017-03-07 14:55:08 +0200

    first commit

--- quickstart/src/app/app.module.ts
+++ quickstart/src/app/app.module.ts
@@ -1,26 +1,20 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
-import { RouterModule }   from '@angular/router';
 
-import { AppComponent }        from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent }     from './heroes.component';
-import { HeroService }         from './hero.service';
-import {DashboardComponent} from "./dashbord.component";
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroesComponent }      from './heroes.component';
+import { HeroService }          from './hero.service';
+
+import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      }
-    ]),
-    //todo src/app/app.module.ts (Dashboard route)
-    DashboardComponent
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
@@ -28,10 +22,7 @@ import {DashboardComponent} from "./dashbord.component";
     HeroDetailComponent,
     HeroesComponent
   ],
-  providers: [
-    HeroService
-  ],
+  providers: [ HeroService ],
   bootstrap: [ AppComponent ]
 })
-export class AppModule {
-}
+export class AppModule { }
